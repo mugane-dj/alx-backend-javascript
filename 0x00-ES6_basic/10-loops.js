6dd83a40603cf76878c9e904acabd10edddfdd9e
mugane <muganes25@gmail.com>
2023-07-24 07:58:40 -0700

    0xA

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+
+  for (const item of array) {
+    newArray.push(appendString + item);
+  }
+
+  return newArray;
+}
