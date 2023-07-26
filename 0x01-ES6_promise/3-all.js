06be6641f8fecfc268d00841c25bba64dbbdb705
mugane <muganes25@gmail.com>
2023-07-25 23:43:59 -0700

    0x3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise = uploadPhoto();
+  const promise1 = createUser();
+  Promise.all([promise, promise1])
+    .then((result) => {
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
