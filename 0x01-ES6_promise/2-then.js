1f0685dca81dc3f4726a9df2143b1283ae14f713
mugane <muganes25@gmail.com>
2023-07-25 23:56:08 -0700

    0x2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'Success' }));
-  promise.catch(() => new Error());
-  promise.finally(() => {
-    console.log('Got a response from the API');
-  });
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => new Error());
 }
