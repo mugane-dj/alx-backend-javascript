27eeb5e073a4f04037f1feb4f701a68139a84c85
mugane-dj <muganedev25@gmail.com>
2023-07-26 15:37:43 +0300

    0x4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,5 @@
-export default function signUpUser(firstName, lastName) {
+function signUpUser(firstName, lastName) {
   return Promise.resolve({ firstName, lastName });
 }
+
+export default signUpUser;
