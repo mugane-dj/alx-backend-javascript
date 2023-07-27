6e7956b51656b46fc37f493a31d82726c52a8355
mugane-dj <muganedev25@gmail.com>
2023-07-27 15:03:03 +0300

    0x1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
+  const array = [19, 20, 34];
   const result = [];
-  const init = new ClassRoom(19);
-  result.push(init);
-  const init1 = new ClassRoom(20);
-  result.push(init1);
-  const init2 = new ClassRoom(34);
-  result.push(init2);
+  array.forEach((element) => {
+    result.push(new ClassRoom(element));
+  });
   return result;
 }
