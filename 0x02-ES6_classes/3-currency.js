b718341f24c6ed8452f321b2950b7a44b358058f
mugane-dj <muganedev25@gmail.com>
2023-07-27 15:56:00 +0300

    0x3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
