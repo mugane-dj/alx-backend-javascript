9d314c56c37bde3a5870f6f39e042d6cd39f177d
mugane-dj <muganedev25@gmail.com>
2023-08-01 15:03:54 +0300

    0xA

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(map) {
-  for (const [item, quantity] of map) {
-    if (quantity === 1) {
-      try {
+  if (map instanceof Map) {
+    for (const [item, quantity] of map) {
+      if (quantity === 1) {
         map.set(item, 100);
-      } catch (error) {
-        throw new Error('Cannot process');
       }
     }
+  } else {
+    throw new Error('Cannot process');
   }
   return map;
 }
