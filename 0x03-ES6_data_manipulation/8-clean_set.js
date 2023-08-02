ffdb35f415dad6c7c708a7dac583a66d924f198a
mugane-dj <muganedev25@gmail.com>
2023-08-02 16:31:04 +0300

    0x8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   let outputString = '';
-  if (startString.length > 1) {
+  if (startString) {
     for (const item of set) {
       if (item.startsWith(startString)) {
         const newItem = item.replace(new RegExp(startString, 'g'), '');
         outputString = `${outputString + newItem}-`;
       }
     }
+    outputString = outputString.slice(0, -1);
   }
-  return outputString.slice(0, -1);
+  return outputString;
 }
