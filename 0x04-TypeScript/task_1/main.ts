682609524baa30da1d8d52470155bfe8218271a7
Samuel Mugane <muganes25@gmail.com>
2023-08-07 10:24:35 +0300

    0x1

--- 0x04-TypeScript/task_1/main.ts
+++
@@ -1,8 +0,0 @@
-interface Teacher {
-    readonly firstName: string; // Only modified when the object is instantiated
-    readonly lastName: string;
-    fullTimeEmployee: boolean; // Always defined
-    yearsOfExperience?: number; // Optional attribute
-    location: string;
-    [propName: string]: boolean | number | string; // Index signature to allow extra attributes
-}
