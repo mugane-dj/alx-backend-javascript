4e4caa0360cb3b1441a5576797e8d22ae5d094fd
mugane-dj <muganedev25@gmail.com>
2023-08-07 11:37:36 +0300

    0x7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -52,3 +52,13 @@ function executeWork (employee: Director | Teacher) {
         console.log(employee.workTeacherTasks());
     }
 }
+
+type Subjects =  'Math' | 'History';
+
+function teachClass (todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
