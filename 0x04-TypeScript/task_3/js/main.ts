3a18777f9d180cd442f3f714e81c488ff8facc38
mugane-dj <muganedev25@gmail.com>
2023-08-07 12:30:05 +0300

    0x8

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,14 @@
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+
+const obj = {
+    firstName: 'Guillaume',
+    lastName: 'Salva'
+}
+
+const newRowID: RowID = CRUD.insertRow(obj);
+const updatedRow: RowElement = {...obj, age: 23};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
