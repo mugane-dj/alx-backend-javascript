d1960e8079c2f94d2473927b8d0cb68729f08a03
mugane-dj <muganedev25@gmail.com>
2023-09-18 17:53:46 +0300

    0x1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (name) => {
   process.stdout.write(`Your name is: ${name.toString()}`);
-  process.exit();
 });
 process.on('exit', () => {
   console.log('This important software is now closing');
