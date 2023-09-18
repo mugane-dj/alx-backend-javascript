119f667e5fcb7be30e5cf8c99b9d4e532eb82114
mugane-dj <muganedev25@gmail.com>
2023-09-18 19:54:48 +0300

    0x2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const studentList = data.split('\n');
+    console.log(`Number of students: ${studentList.length - 1}`);
+    const studentDict = {};
+    for (let i = 1; i < studentList.length; i += 1) {
+      const values = studentList[i].split(',');
+      const field = values[3];
+      const firstName = values[0];
+
+      if (field === 'SWE' || field === 'CS') {
+        if (!studentDict[field]) {
+          studentDict[field] = {
+            students: [],
+            count: 0,
+          };
+        }
+
+        studentDict[field].students.push(firstName);
+        studentDict[field].count += 1;
+      }
+    }
+
+    for (const key of Object.keys(studentDict)) {
+      const studentsList = studentDict[key].students.join(', ');
+      console.log(`Number of students in ${key}: ${studentDict[key].count}. List: ${studentsList}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
