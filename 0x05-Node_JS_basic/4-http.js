0c7198ec1ddc9f9e2d9ac909c50b48b75dfac95f
mugane-dj <muganedev25@gmail.com>
2023-09-19 10:37:12 +0300

    0x4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,7 @@
+// Create a small HTTP server using the http module
+const app = require('http');
+
+app.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
