d870c708c05035ab87ab4b79b13ac886a013a832
mugane-dj <muganedev25@gmail.com>
2023-09-19 10:33:41 +0300

    0x1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+// Read user input and write it to STDOUT
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (name) => {
   process.stdout.write(`Your name is: ${name.toString()}`);
