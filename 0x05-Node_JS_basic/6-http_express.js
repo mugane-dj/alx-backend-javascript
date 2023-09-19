445d190ed65d3bf233f8be3f37cbd1a045532048
mugane-dj <muganedev25@gmail.com>
2023-09-19 13:05:21 +0300

    0x6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+// Create a small HTTP server using Express
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+}).listen(1245);
+
+module.exports = app;
