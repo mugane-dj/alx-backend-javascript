a0a3a42ac3c81693b4066503a7ecd32e5851593a
mugane-dj <muganedev25@gmail.com>
2023-09-21 16:21:44 +0300

    0x8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+// Test suite for express server
+const app = require('./api.js')
+const chai = require('chai')
+const request = require('request')
+
+const expect = chai.expect
+const PORT = 7865
+
+const baseUrl = `http://localhost:${PORT}`
+
+describe('app', () => {
+  it('should return "Welcome to the payment system" when GET /', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200)
+      expect(body).to.equal('Welcome to the payment system')
+      done()
+    })
+  })
+})
