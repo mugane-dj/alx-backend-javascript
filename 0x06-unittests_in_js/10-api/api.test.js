59776d40872a451fdb776df8d8809a6c2e3cc795
mugane-dj <muganedev25@gmail.com>
2023-09-21 18:40:02 +0300

    0xA

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -30,7 +30,7 @@ describe('Index page', () => {
   });
 
   it('should return correct response when called', done => {
-    request.post(`${baseUrl}/login {json: {userName: 'Betty'}}`, (err, res, body) => {
+    request.post(`${baseUrl}/login`, {json: {userName: 'Betty'}}, (err, res, body) => {
         expect(res.statusCode).to.be.equal(200);
         expect(body).to.be.equal('Welcome Betty');
         done();
