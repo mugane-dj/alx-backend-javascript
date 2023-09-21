b74bc047b033ac0d59e91c4e380219389d4ccf80
mugane-dj <muganedev25@gmail.com>
2023-09-21 18:45:48 +0300

    0xA

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -31,16 +31,16 @@ describe('Index page', () => {
 
   it('should return correct response when called', done => {
     request.post(`${baseUrl}/login`, {json: {userName: 'Betty'}}, (err, res, body) => {
-        expect(res.statusCode).to.be.equal(200);
-        expect(body).to.be.equal('Welcome Betty');
-        done();
-      });
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Betty');
+      done();
+    });
   });
 
   it('should return correct obj response when called', done => {
     request.get(`${baseUrl}/available_payments`, (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(JSON.parse(body)).to.be.deep.equal({ payment_methods: {credit_cards: true, paypal: false } });
+      expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
       done();
     });
   });
