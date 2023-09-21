a87c9ae0dfc28f0d85315ec686fc16cee30ed095
mugane-dj <muganedev25@gmail.com>
2023-09-21 14:43:34 +0300

    0x1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-undef */
 // Tests for calculateNumber
-const assert = require('assert');
-const calculateNumber = require("./1-calcul.js");
+const assert = require('assert')
+const calculateNumber = require('./1-calcul.js')
 
 describe('CalculateNumber()', () => {
-    const tests = [
-        {args: ['SUM', 1.4, 4.5], expected: 6},
-        {args: ['SUBTRACT', 1.4, 4.5], expected: -4},
-        {args: ['DIVIDE', 1.4, 4.5], expected: 0.2},
-        {args: ['DIVIDE', 1.4, 0], expected: 'Error'},
-    ];
+  const tests = [
+    { args: ['SUM', 1.4, 4.5], expected: 6 },
+    { args: ['SUBTRACT', 1.4, 4.5], expected: -4 },
+    { args: ['DIVIDE', 1.4, 4.5], expected: 0.2 },
+    { args: ['DIVIDE', 1.4, 0], expected: 'Error' }
+  ]
 
-    tests.forEach((test) => {
-        it('correctly ' + test.args[0] + 'S ' + test.args[1] + ' and ' + test.args[2], () => {
-            const res = calculateNumber.apply(null, test.args);
-            assert.equal(res, test.expected);
-        });
-    });
-});
+  tests.forEach((test) => {
+    it('correctly ' + test.args[0] + 'S ' + test.args[1] + ' and ' + test.args[2], () => {
+      const res = calculateNumber.apply(null, test.args)
+      assert.equal(res, test.expected)
+    })
+  })
+})
