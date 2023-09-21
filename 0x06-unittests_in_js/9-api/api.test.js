7d19246c2534621735ec71cbdca16faa113702fb
mugane-dj <muganedev25@gmail.com>
2023-09-21 17:36:39 +0300

    0x9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+// Test suite for express server
+const request = require('request')
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  const baseUrl = 'http://localhost:7865';
+
+  it('should return correct response when called', done => {
+    request.get(`${baseUrl}/`, (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome to the payment system');
+        done();
+    });
+  });
+
+  it('should return correct status code when id is a number', done => {
+    request.get(`${baseUrl}/cart/82`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 82');
+    });
+  });
+
+  it('should return correct status code when id is not a number', done => {
+    request.get(`${baseUrl}/cart/82`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+    });
+  });
+});
