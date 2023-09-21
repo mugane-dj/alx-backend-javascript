a3ff5678667e93289b299694d4fe598a275ca36e
mugane-dj <muganedev25@gmail.com>
2023-09-21 18:20:22 +0300

    0xA

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,47 @@
+// Test suite for express server
+const request = require('request')
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Index page', () => {
+  const baseUrl = 'http://localhost:7865';
+
+  it.skip('should return correct response when called', done => {
+    request.get(`${baseUrl}/`, (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome to the payment system');
+        done();
+    });
+  });
+
+  it.skip('should return correct status code when id is a number', done => {
+    request.get(`${baseUrl}/cart/82`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 82');
+      done();
+    });
+  });
+
+  it.skip('should return correct status code when id is not a number', done => {
+    request.get(`${baseUrl}/cart/hello`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('should return correct response when called', done => {
+    request.post(`${baseUrl}/login {json: {userName: 'Betty'}}`, (err, res, body) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome Betty');
+        done();
+      });
+  });
+
+  it('should return correct obj response when called', done => {
+    request.get(`${baseUrl}/available_payments`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
+    });
+  });
+});
