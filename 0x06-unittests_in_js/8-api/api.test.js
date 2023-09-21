7509c7eb3510fc2c6720502881b9c155676de1c4
mugane-dj <muganedev25@gmail.com>
2023-09-21 16:28:40 +0300

    0x8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -11,8 +11,8 @@ const baseUrl = `http://localhost:${PORT}`
 describe('app', () => {
   it('should return "Welcome to the payment system" when GET /', (done) => {
     request.get(baseUrl, (error, response, body) => {
-      expect(response.statusCode).to.equal(200)
-      expect(body).to.equal('Welcome to the payment system')
+      expect(response.statusCode).to.be.equal(200)
+      expect(body).to.be.equal('Welcome to the payment system')
       done()
     })
   })
