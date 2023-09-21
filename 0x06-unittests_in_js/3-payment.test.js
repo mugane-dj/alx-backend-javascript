42376bd7de3accd8ea94a8d46490dbc4f72d517d
mugane-dj <muganedev25@gmail.com>
2023-09-21 14:55:00 +0300

    0x3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -10,9 +10,9 @@ const expect = chai.expect
 describe('sendPaymentRequestToApi', () => {
   it('correctly returns shipping total', () => {
     const spy = sinon.spy(Utils, 'calculateNumber')
-    const consoleSpy = sinon.spy(console, 'log')
+
     sendPaymentRequestToApi(100, 20)
     expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true
-    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true
+    spy.restore()
   })
 })
