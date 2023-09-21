c25fe294249ea62a1da5e608e0ea780b71f4ccf4
mugane-dj <muganedev25@gmail.com>
2023-09-21 13:59:28 +0300

    0x1

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+// Sum function
+
+const calculateNumber = (type, a, b) => {
+    if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+        if (Math.round(b) !== 0) {
+            return Math.round(a) / Math.round(b);
+        } else {
+            return 'Error';
+        }
+    }
+    
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,19 @@
+// Tests for calculateNumber
+const assert = require('assert');
+const calculateNumber = require("./1-calcul.js");
+
+describe('CalculateNumber()', () => {
+    const tests = [
+        {args: ['SUM', 1.4, 4.5], expected: 6},
+        {args: ['SUBTRACT', 1.4, 4.5], expected: -4},
+        {args: ['DIVIDE', 1.4, 4.5], expected: 0.2},
+        {args: ['DIVIDE', 1.4, 0], expected: 'Error'},
+    ];
+
+    tests.forEach((test) => {
+        it('correctly ' + test.args[0] + 'S ' + test.args[1] + ' and ' + test.args[2], () => {
+            const res = calculateNumber.apply(null, test.args);
+            assert.equal(res, test.expected);
+        });
+    });
+});
