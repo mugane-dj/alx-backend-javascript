d428c855e98174ca236ac43dde3bfea3de00e87d
mugane-dj <muganedev25@gmail.com>
2023-09-21 14:40:37 +0300

    0x1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,18 +1,17 @@
 // Sum function
 
 const calculateNumber = (type, a, b) => {
-    if (type === 'SUM') {
-        return Math.round(a) + Math.round(b);
-    } else if (type === 'SUBTRACT') {
-        return Math.round(a) - Math.round(b);
-    } else if (type === 'DIVIDE') {
-        if (Math.round(b) !== 0) {
-            return Math.round(a) / Math.round(b);
-        } else {
-            return 'Error';
-        }
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b)
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b)
+  } else if (type === 'DIVIDE') {
+    if (Math.round(b) !== 0) {
+      return Math.round(a) / Math.round(b)
+    } else {
+      return 'Error'
     }
-    
+  }
 }
 
-module.exports = calculateNumber;
+module.exports = calculateNumber
